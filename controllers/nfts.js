2edfc0d3421ffaf9dd28f9dfb32747b6a3dfe6e5
jvalente <jvalente96@gmail.com>
2023-02-22 21:05:36 +0000

    add nfts

--- controllers/nfts.js
+++ controllers/nfts.js
@@ -1,5 +1,6 @@
 import { Network } from "alchemy-sdk";
 import { Alchemy } from "alchemy-sdk";
+import { getMessage } from "eip-712";
 import { config } from "dotenv";
 config();
 
@@ -34,4 +35,260 @@ export async function getImageURL(req, res) {
   } else {
     res.status(200).json("");
   }
+}
+
+export async function getFloorPrice(req, res) {
+  const { address, chainId } = req.query;
+  var floorPrice = "0";
+
+  console.log("Got a price request for chainID:", chainId);
+  if (!(address && chainId)) {
+    //Check inputs
+    res.status(400).json({ error: "Lacks input data" });
+  }
+
+  // Test collections case for goerli
+  if (chainId == "5") {
+    floorPrice = "8000000000000000"; //Price of 0.008 ETH
+    // Mainnet Case
+  } else {
+    const options = {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "x-api-key": process.env.UPSHOT_API_KEY,
+      },
+    };
+
+    const url = "https://api.upshot.xyz/v2/collections/" + address;
+
+    const collectionResponse = await fetch(url, options).catch((err) =>
+      console.error(err)
+    );
+    const collection = await collectionResponse.json();
+
+    //Build return data
+    console.log(collection);
+    if (collection.data) {
+      floorPrice = collection.data.floor.wei;
+    }
+  }
+  res.status(200).json(floorPrice);
+}
+
+export async function getPrice(req, res) {
+  const { requestId, collection, tokenIds, chainId } = req.query;
+  const tokensIdsArray = tokenIds.split(",");
+  const expiryTimestamp = Math.round(Date.now() / 1000) + ONE_HOUR;
+  var priceSum = 0;
+
+  console.log("Got a price request for chainID:", chainId);
+  if (!(collection && chainId)) {
+    //Check inputs
+    res.status(400).json({ error: "Lacks input data" });
+  }
+
+  const addresses =
+    chainId in contractAddresses
+      ? contractAddresses[chainId]
+      : contractAddresses["1"];
+
+  // Test collections case for goerli
+  if (chainId == "5") {
+    priceSum = BigNumber.from("8000000000000000")
+      .mul(tokensIdsArray.length)
+      .toString(); //Price of 0.008 ETH per Token
+
+    // Mainnet Case
+  } else {
+    const options = {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "x-api-key": process.env.UPSHOT_API_KEY,
+      },
+    };
+
+    const url = "https://api.upshot.xyz/v2/collections/" + collection;
+
+    const collectionResponse = await fetch(url, options).catch((err) =>
+      console.error(err)
+    );
+    const collection = await collectionResponse.json();
+    if (collection.data.floor !== undefined) {
+      priceSum = BigNumber.from(collection.data.floor.wei)
+        .mul(tokensIdsArray.length)
+        .toString();
+    }
+  }
+
+  if (requestId && priceSum != 0) {
+    const payload = abi.encodeParameter(
+      {
+        AssetsPrice: {
+          collection: "address",
+          tokenIds: "uint256[]",
+          amount: "uint256",
+        },
+      },
+      {
+        collection: collection,
+        tokenIds: tokensIdsArray,
+        amount: priceSum,
+      }
+    );
+
+    //Sign the payload and build the packet
+    const typedData = {
+      types: {
+        EIP712Domain: [
+          { name: "name", type: "string" },
+          { name: "version", type: "string" },
+          { name: "chainId", type: "uint256" },
+          { name: "verifyingContract", type: "address" },
+        ],
+        VerifyPacket: [
+          { name: "request", type: "bytes32" },
+          { name: "deadline", type: "uint256" },
+          { name: "payload", type: "bytes" },
+        ],
+      },
+      primaryType: "VerifyPacket",
+      domain: {
+        name: "leNFT",
+        version: "1",
+        chainId: chainId,
+        verifyingContract: addresses.NFTOracle,
+      },
+      message: {
+        request: requestId,
+        deadline: expiryTimestamp,
+        payload: payload,
+      },
+    };
+
+    const signingKey = new utils.SigningKey(process.env.SERVER_PRIVATE_KEY);
+
+    // Get a signable message from the typed data
+    const message = getMessage(typedData, true);
+
+    // Sign the message with the private key
+    const { r, s, v } = signingKey.signDigest(message);
+
+    const sigPacket = {
+      v: v,
+      r: r,
+      s: s,
+      request: requestId,
+      deadline: expiryTimestamp,
+      payload: payload,
+    };
+
+    res.status(200).json({ sig: sigPacket, price: priceSum });
+  } else {
+    res.status(200).json({ price: priceSum });
+  }
+}
+
+export async function getAddressNFTs(req, res) {
+  const { address, collection, chainId } = req.query;
+
+  var chainName;
+  console.log(chainId);
+  if (chainId == 1) {
+    chainName = "eth";
+  } else if (chainId == 5) {
+    chainName = "goerli";
+  } else {
+    res.status(400).json({ error: "Invalid chainId" });
+  }
+
+  var collectionsURLString = "";
+  if (collection) {
+    collectionsURLString = "&contractAddresses[]=" + collection;
+  }
+
+  const url =
+    "https://eth-" +
+    chainName +
+    ".g.alchemy.com/nft/v2/" +
+    process.env.ALCHEMY_API_KEY +
+    "/getNFTs";
+
+  const options = {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  var nextPageExists = true;
+  var returnNFTs = [];
+  var pageKey;
+  var nftsPageResponse = {};
+  var pageKeyURLString;
+
+  while (nextPageExists) {
+    pageKeyURLString = "";
+
+    if (nextPageExists) {
+      pageKeyURLString = "&pageKey=" + pageKey;
+    }
+    nftsPageResponse = await fetch(
+      url + "?owner=" + address + collectionsURLString + pageKeyURLString,
+      options
+    ).catch((err) => console.error(err));
+    const nftsPage = await nftsPageResponse.json();
+
+    // Merge the arrays
+    returnNFTs = returnNFTs.concat(nftsPage.ownedNfts);
+
+    if (nftsPage.pageKey !== undefined) {
+      pageKey = nftsPage.pageKey;
+      nextPageExists = true;
+    } else {
+      nextPageExists = false;
+    }
+  }
+
+  console.log("nfts", returnNFTs);
+
+  res.status(200).json(returnNFTs);
+}
+
+export async function getAddressCollections(req, res) {
+  const { address, chainId } = req.query;
+
+  var chainName;
+  console.log(chainId);
+  if (chainId == 1) {
+    chainName = "eth";
+  } else if (chainId == 5) {
+    chainName = "goerli";
+  } else {
+    res.status(400).json({ error: "Invalid chainId" });
+  }
+
+  const url =
+    "https://eth-" +
+    chainName +
+    ".g.alchemy.com/nft/v2/" +
+    process.env.ALCHEMY_API_KEY +
+    "/getContractsForOwner";
+
+  const options = {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  };
+  const getNFTCollectionsResponse = await fetch(
+    url + "?owner=" + address,
+    options
+  ).catch((err) => console.error(err));
+  const nftCollections = await getNFTCollectionsResponse.json();
+
+  console.log("nftCollections", nftCollections.contracts);
+
+  res.status(200).json(nftCollections.contracts);
 }
--- routes/nfts.js
+++ routes/nfts.js
@@ -1,7 +1,17 @@
 import express from "express";
 var router = express.Router();
-import { getImageURL } from "../controllers/nfts.js";
+import {
+  getImageURL,
+  getFloorPrice,
+  getPrice,
+  getAddressNFTs,
+  getAddressCollections,
+} from "../controllers/nfts.js";
 
+router.get("/floorPrice", getFloorPrice);
+router.get("/price", getPrice);
+router.get("/address", getAddressNFTs);
+router.get("/addressCollections", getAddressCollections);
 router.get("/imageURL", getImageURL);
 
 export default router;
